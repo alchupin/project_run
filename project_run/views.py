2388cf49e3915eaba0976765c50f56e1236b6b0e
Aleksey Chupin <aleksey.chupin@x5.ru>
2025-10-12 06:54:30 +0300

    fix

--- project_run/views.py
+++ project_run/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from django.conf import setting 
+from django.conf import settings
 
 @api_view(['GET'])
 def get_org_info(request):
