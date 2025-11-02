170188d7ba7817441e6f61f3f7e7efcdddb620a6
Aleksey Chupin <aleksey.chupin@x5.ru>
2025-11-02 14:37:47 +0300

    создание атлета

--- app_run/views.py
+++ app_run/views.py
@@ -6,3 +6,6 @@ from .serializers import RunSerializer
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.select_related('athlete').all()
     serializer_class = RunSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(athlete=self.request.user)
