68d7eb5f6a82c3498766b099aad759bfff3dfa04
Aleksey Chupin <aleksey.chupin@x5.ru>
2025-11-02 14:42:04 +0300

    создание атлета

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -9,8 +9,19 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'first_name', 'last_name']
 
+from django.contrib.auth.models import AnonymousUser
+
 class RunSerializer(serializers.ModelSerializer):
     athlete = UserSerializer(read_only=True)
     class Meta:
         model = Run
         fields = '__all__'
+    
+    def create(self, validated_data):
+        # Если athlete не передан, используем текущего пользователя
+        request = self.context.get('request', None)
+        if 'athlete' not in validated_data and request and hasattr(request, 'user'):
+            if isinstance(request.user, AnonymousUser):
+                raise serializers.ValidationError("Аутентификация обязательна для создания записи")
+            validated_data['athlete'] = request.user
+        return super().create(validated_data)
