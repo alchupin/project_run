22bc535cb9ffb7deb34a0e62996df6f7e8707afa
Aleksey Chupin <aleksey.chupin@x5.ru>
2025-11-02 14:48:34 +0300

    fix

--- app_run/views.py
+++ app_run/views.py
@@ -3,22 +3,6 @@ from .models import Run
 from .serializers import RunSerializer
 
 
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import AnonymousUser
-from .models import Run
-from .serializers import RunSerializer
-
-
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.select_related('athlete').all()
     serializer_class = RunSerializer
-    permission_classes = [IsAuthenticated]  # Требуем аутентификацию для всех действий
-    
-    def perform_create(self, serializer):
-        # Проверяем, что пользователь аутентифицирован
-        if isinstance(self.request.user, AnonymousUser):
-            raise ValueError("Аутентификация обязательна для создания записи")
-        serializer.save(athlete=self.request.user)
